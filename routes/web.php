8647694cd08fb1ea197c79b8ae5b8019f336b97c
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-06-03 18:40:00 +0530

    chore: Add admin routes for roles and permissions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +27,13 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+
+    Route::name('admin.')->middleware('role:admin')->prefix('admin')->group(function () {
+        Route::get('/', function () {
+            return view('admin.index');
+        })->name('dashboard'); // Ensure this matches 'admin.dashboard'
+        Route::resource('/roles', RoleController::class);
+        Route::resource('/permissions', PermissionController::class);
+    });
 });
